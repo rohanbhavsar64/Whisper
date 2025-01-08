738e3eb6f8887a9cbce4841384bb29d23573cf5f
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2025-01-08 11:14:18 +0530

    Update whisper.py

--- whisper.py
+++ whisper.py
@@ -2,7 +2,7 @@ import streamlit as st
 import whisper 
 st.title('Transcripter App')
 audio=st.file_uploader('Upload Audio File',type=['wav','mp3','m4a','mp4'])
-model=whisper.load_model('base')
+model=whisper.load_model['base']
 if st.button('Transcribe'):
   if audio is not None:
     t=model.transcribe(audio.name)
