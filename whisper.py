2aa68f5ea5748db2a19e5a91e7950140e61e1226
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2025-01-20 12:02:23 +0530

    Update whisper.py

--- whisper.py
+++ whisper.py
@@ -1,7 +1,7 @@
 import streamlit as st
+import whisper
 st.title('Transcripter App')
 audio=st.file_uploader('Upload Audio File')
-import whisper
 model = whisper.load_model('base')
 if audio:
   t=model.transcribe(audio)
