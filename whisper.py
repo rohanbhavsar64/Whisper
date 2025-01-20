5ac45956c04898b80260cf294d416700e8a61c0f
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2025-01-20 11:58:22 +0530

    Update whisper.py

--- whisper.py
+++ whisper.py
@@ -4,6 +4,6 @@ audio=st.file_uploader('Upload Audio File')
 import whisper
 model = whisper.load_model('base')
 if st.button('Transcribe'):
-  if audio is not None:
+  if audio:
     t=model.transcribe(audio.name)
     st.markdown(t['text'])
